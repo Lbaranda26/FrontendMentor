26ac4da924aa619250d938eab6b0862d300abf75
Lbaranda26 <64288089+Lbaranda26@users.noreply.github.com>
2024-05-22 01:27:33 +0800

    update

---
+++ INTERMEDIATE/calculator-app-main/script.js
@@ -0,0 +1,3 @@
+console.log(document.styleSheets[0].href = "http://127.0.0.1:5500/INTERMEDIATE/calculator-app-main/style2.css");
+document.styleSheets[0].href = "http://127.0.0.1:5500/INTERMEDIATE/calculator-app-main/style2.css";
+console.log("Hello World!");
